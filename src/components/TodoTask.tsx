93647bb0c972fa36e2413d65c69daee32180c9d7
Kevin Do <kevind0501@gmail.com>
2022-06-24 18:00:01 -0700

    styling

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { ITask } from "../Interfaces";
+import "../App.css";
 
 interface Props {
   task: ITask;
 }
 const TodoTask = ({ task }: Props) => {
-  return <div>{task.taskName}</div>;
+  return (
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button>Delete</button>
+    </div>
+  );
 };
 
 export default TodoTask;
